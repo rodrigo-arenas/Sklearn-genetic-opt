1323ae9a2a64f8ba5a7bb733ed3b40a7318a29e3
rodrigoarenas456 <31422766+rodrigoarenas456@users.noreply.github.com>
2021-04-26 20:16:39 -0500

    Release 0.1.0 requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name="sklearn-genetic",
+    name="sklearn-genetic-opt",
     version="0.1.0",
     description="Sklearn models hyperparameters tuning using genetic algorithms",
     long_description=README,
@@ -24,13 +24,10 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+
     ],
     packages=find_packages(include=['sklearn_genetic', 'sklearn_genetic.*']),
-    install_requires=[
-        'scikit-learn>=0.20.1',
-        'numpy'
-    ],
+    install_requires=['scikit-learn>=0.21.3', 'numpy>=1.13.3'],
     python_requires=">=3.6",
     include_package_data=True,
 )
