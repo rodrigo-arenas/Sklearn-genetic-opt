9d6464101570c7a32e6fac1492ba2b650c0d8e3a
rodrigoarenas456 <31422766+rodrigoarenas456@users.noreply.github.com>
2021-04-27 15:41:28 -0500

    Added more tests

--- sklearn_genetic/tests/test_genetic_search.py
+++ sklearn_genetic/tests/test_genetic_search.py
@@ -1,10 +1,12 @@
 from sklearn_genetic import GASearchCV
-from sklearn.datasets import load_digits
+from sklearn.datasets import load_digits, load_boston
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.utils.validation import check_is_fitted
-
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.cluster import KMeans
+import pytest
 
 data = load_digits()
 label_names = data['target_names']
@@ -89,3 +91,79 @@ def test_expected_ga_no_categorical():
     assert 'max_depth' in evolved_estimator.best_params_
     assert 'max_leaf_nodes' in evolved_estimator.best_params_
     assert len(evolved_estimator._best_solutions) == generations
+
+
+def test_negative_criteria():
+    data_boston = load_boston()
+
+    y_boston = data['target']
+    X_boston = data['data']
+
+    X_train_b, X_test_b, y_train_b, y_test_b = train_test_split(X_boston, y_boston, test_size=0.33, random_state=42)
+
+    clf = DecisionTreeRegressor()
+
+    evolved_estimator = GASearchCV(clf,
+                                   cv=3,
+                                   scoring='max_error',
+                                   population_size=16,
+                                   generations=30,
+                                   tournament_size=3,
+                                   elitism=True,
+                                   crossover_probability=0.9,
+                                   mutation_probability=0.05,
+                                   continuous_parameters={'ccp_alpha': (0, 1)},
+                                   categorical_parameters={'criterion': ['mse', 'mae']},
+                                   integer_parameters={'max_depth': (2, 20), 'min_samples_split': (2, 30)},
+                                   encoding_length=20,
+                                   criteria='min',
+                                   n_jobs=-1)
+
+    evolved_estimator.fit(X_train_b, y_train_b)
+
+    assert check_is_fitted(evolved_estimator) is None
+    assert 'ccp_alpha' in evolved_estimator.best_params_
+    assert 'criterion' in evolved_estimator.best_params_
+    assert 'max_depth' in evolved_estimator.best_params_
+    assert 'min_samples_split' in evolved_estimator.best_params_
+    assert len(evolved_estimator._best_solutions) == 30
+    assert len(evolved_estimator.predict(X_test_b)) == len(X_test)
+    assert evolved_estimator.score(X_train_b, y_train_b) >= 0
+
+
+def test_wrong_criteria():
+    clf = SGDClassifier(loss='log', fit_intercept=True)
+    generations = 8
+    with pytest.raises(Exception) as excinfo:
+        evolved_estimator = GASearchCV(clf,
+                                       cv=3,
+                                       scoring='accuracy',
+                                       population_size=12,
+                                       generations=generations,
+                                       tournament_size=3,
+                                       elitism=False,
+                                       continuous_parameters={'l1_ratio': (0, 1), 'alpha': (1e-4, 1)},
+                                       categorical_parameters={'average': [True, False]},
+                                       verbose=False,
+                                       criteria='maximization',
+                                       encoding_length=10)
+    assert str(excinfo.value) == "Criteria must be 'max' or 'min', got maximization instead"
+
+
+def test_wrong_estimator():
+    clf = KMeans()
+    generations = 8
+    with pytest.raises(Exception) as excinfo:
+        evolved_estimator = GASearchCV(clf,
+                                       cv=3,
+                                       scoring='accuracy',
+                                       population_size=12,
+                                       generations=generations,
+                                       tournament_size=3,
+                                       elitism=False,
+                                       continuous_parameters={'l1_ratio': (0, 1), 'alpha': (1e-4, 1)},
+                                       categorical_parameters={'average': [True, False]},
+                                       verbose=False,
+                                       criteria='maximization',
+                                       encoding_length=10)
+    assert str(excinfo.value) == "KMeans() is not a valid Sklearn classifier or regressor"
