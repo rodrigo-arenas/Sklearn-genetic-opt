6229dfaead07111478661280c4a2a78b272f3a40
rodrigoarenas456 <31422766+rodrigoarenas456@users.noreply.github.com>
2021-05-28 15:45:11 -0500

    test plot space function

--- sklearn_genetic/tests/test_utils.py
+++ sklearn_genetic/tests/test_utils.py
@@ -1,8 +1,11 @@
-from sklearn_genetic.utils import plot_fitness_evolution
-from sklearn_genetic import GASearchCV
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+
+from .. import GASearchCV
+from ..utils import plot_fitness_evolution, plot_search_space
+from ..space import Integer, Categorical, Continuous
+
 
 data = load_boston()
 
@@ -14,23 +17,31 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 clf = DecisionTreeRegressor()
 
 evolved_estimator = GASearchCV(clf,
-                               cv=3,
+                               cv=2,
                                scoring='r2',
                                population_size=8,
-                               generations=60,
+                               generations=5,
                                tournament_size=3,
                                elitism=True,
                                crossover_probability=0.9,
                                mutation_probability=0.05,
-                               continuous_parameters={'ccp_alpha': (0, 1)},
-                               categorical_parameters={'criterion': ['mse', 'mae']},
-                               integer_parameters={'max_depth': (2, 20), 'min_samples_split': (2, 30)},
+                               param_grid={'ccp_alpha': Continuous(0, 1),
+                                           'criterion': Categorical(['mse', 'mae']),
+                                           'max_depth': Integer(2, 20),
+                                           'min_samples_split': Integer(2, 30)},
                                criteria='max',
                                n_jobs=-1)
 
 evolved_estimator.fit(X_train, y_train)
 
 
-def test_plot_created():
+def test_plot_evolution():
     plot = plot_fitness_evolution(evolved_estimator)
+
+
+def test_plot_space():
+    plot = plot_search_space(evolved_estimator)
+    plot = plot_search_space(evolved_estimator, features=['ccp_alpha', 'max_depth', 'min_samples_split'])
+
+
 
