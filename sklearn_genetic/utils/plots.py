916e790fecf73513ebf47ac55b9e9e443a02c6f9
rodrigoarenas456 <31422766+rodrigoarenas456@users.noreply.github.com>
2021-05-28 15:29:05 -0500

    changed default parameters

--- sklearn_genetic/utils/plots.py
+++ sklearn_genetic/utils/plots.py
@@ -30,10 +30,14 @@ def plot_fitness_evolution(estimator):
     return ax
 
 
-def plot_search_space(estimator, features: list = None):
+def plot_search_space(estimator, height=2, s=25, features: list = None):
     """
     Parameters
     ----------
+    height: float, default=2
+        Height if each facet
+    s: float, default=5
+        Size of the markers in scatter plot
     features: list, default=None
         Subset of features to plot, if 'None' it plots all the features by default
     estimator: Fitted GASearchCV
@@ -49,8 +53,8 @@ def plot_search_space(estimator, features: list = None):
     else:
         stats = df[estimator.space.parameters]
 
-    g = sns.PairGrid(stats)
-    g = g.map_upper(sns.scatterplot)
+    g = sns.PairGrid(stats, diag_sharey=False, height=height)
+    g = g.map_upper(sns.scatterplot, s=s)
     g = g.map_lower(sns.kdeplot, shade=True)
     g = g.map_diag(sns.kdeplot, shade=True)
     return g
