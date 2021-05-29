7da7d53ba44110334f5209c8524dec718de35850
rodrigoarenas456 <31422766+rodrigoarenas456@users.noreply.github.com>
2021-05-28 23:41:06 -0500

    Callbacks import

--- sklearn_genetic/__init__.py
+++ sklearn_genetic/__init__.py
@@ -1,4 +1,5 @@
 from .genetic_search import GASearchCV
 from .plots import plot_fitness_evolution, plot_search_space
+from .callbacks import ThresholdStopping, ConsecutiveStopping
 
-__all__ = ['GASearchCV', 'plot_fitness_evolution', 'plot_search_space']
+__all__ = ['GASearchCV', 'plot_fitness_evolution', 'plot_search_space', 'ThresholdStopping', 'ConsecutiveStopping']
