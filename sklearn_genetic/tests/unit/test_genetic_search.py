7098324eddbbd95f642224fd4a1b345a02b52f4f
Rodrigo Arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2021-12-08 09:32:01 -0500

    Relative imports

--- sklearn_genetic/tests/unit/test_genetic_search.py
+++ sklearn_genetic/tests/unit/test_genetic_search.py
@@ -10,9 +10,9 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-from sklearn_genetic import GASearchCV
-from sklearn_genetic.space import Integer, Categorical, Continuous
-from sklearn_genetic.callbacks import (
+from .. import GASearchCV
+from ..space import Integer, Categorical, Continuous
+from ..callbacks import (
     ThresholdStopping,
     DeltaThreshold,
     ConsecutiveStopping,
