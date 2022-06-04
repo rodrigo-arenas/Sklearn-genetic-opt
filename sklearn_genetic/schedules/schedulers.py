87391bcc019d52a76fc354ac63bc570121eb8ae7
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2022-06-04 09:54:36 -0500

    Simpler formulas

--- sklearn_genetic/schedules/schedulers.py
+++ sklearn_genetic/schedules/schedulers.py
@@ -3,30 +3,23 @@ from .base import BaseScheduler
 
 
 class ExponentialDecay(BaseScheduler):
-    def __init__(self, initial_value, decay_rate, min_value=None):
+    def __init__(self, initial_value, decay_rate, min_value=0.0):
         super().__init__(initial_value, min_value, decay_rate)
 
     def step(self, *args, **kwargs):
-        self.current_value = self.initial_value * math.exp(
-            -self.decay_rate * self.current_step
-        )
+        self.current_value = ((self.initial_value - self.min_value) * math.exp(
+            -self.decay_rate * self.current_step)) + self.min_value
         self.current_step += 1
-        if self.min_value:
-            self.current_value = max(self.current_value, self.min_value)
 
         return self.current_value
 
 
 class InverseDecay(BaseScheduler):
-    def __init__(self, initial_value, decay_rate, min_value=None):
+    def __init__(self, initial_value, decay_rate, min_value=0.0):
         super().__init__(initial_value, min_value, decay_rate)
 
     def step(self, *args, **kwargs):
-        self.current_value = self.initial_value / (
-            1 + self.decay_rate * self.current_step
-        )
+        self.current_value = self.min_value + ((self.initial_value - self.min_value) / (1 + self.decay_rate * self.current_step))
         self.current_step += 1
-        if self.min_value:
-            self.current_value = max(self.current_value, self.min_value)
 
         return self.current_value
