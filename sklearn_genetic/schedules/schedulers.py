8c3168d0b5680a191bc38b0af06702176615b23f
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2022-06-04 15:30:18 -0500

    fix rate behavior

--- sklearn_genetic/schedules/schedulers.py
+++ sklearn_genetic/schedules/schedulers.py
@@ -90,7 +90,7 @@ class PotentialAdapter(BaseAdapter):
         super().__init__(initial_value, end_value, adaptive_rate)
 
     def step(self):
-        self.current_value = self.end_value + (self.initial_value - self.end_value)*math.pow(self.adaptive_rate, self.current_step)
+        self.current_value = self.end_value + (self.initial_value - self.end_value)*math.pow((1-self.adaptive_rate), self.current_step)
         self.current_step += 1
 
         return self.current_value
